d607d498904d63b65e02334644994335c60c21c9
Ashutosh <ashutosh141619@gmail.com>
2023-10-01 23:01:14 +0530

    solved issues with templates and created new pipelines

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,13 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-   path("", views.index, name="ShopHome")
+   path("", views.index, name="ShopHome"),
+   path("about/", views.about, name="AboutUs"),
+   path("contact/", views.contact, name="ContactUs"),
+   path("tracker/", views.tracker, name="TrackingStatus"),
+   path("search/", views.search, name="Search"),
+   path("productView/", views.productView, name="prodView"),
+   path("checkout/", views.checkout, name="Checkout"),
+   path("cart/", views.cart, name="cart"),
+   path("reviews/", views.reviews, name="reviews"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,27 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'shop/index.html')
+
+def about(request):
+    return HttpResponse("About Page")
+
+def contact(request):
+    return HttpResponse("Contact Page")
+
+def tracker(request):
+    return HttpResponse("Tracker Page")
+
+def search(request):
+    return HttpResponse("Search Page")
+
+def productView(request):
+    return HttpResponse("Product View Page")
+
+def checkout(request):
+    return HttpResponse("Checkout Page")
+
+def cart(request):
+    return HttpResponse("Cart Page")
+
+def reviews(request):
+    return HttpResponse("Reviews Page")
